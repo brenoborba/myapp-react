02629f6ed24942e1083ef780d1a3bceb60de14b9
Breno Bessa Borba Alcantara <a40541@alunos.ipb.pt>
2022-07-26 15:22:17 +0100

    first setup

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import Header from './partials/Header/Header.js'
+
+const Home = () =>  {
+  return (
+    <Header />
+
+  )
+}
+
+export default Home;
---
+++ src/partials/Header/Header.style.js
@@ -0,0 +1,13 @@
+/* Criar estilos do componente JSS (JavaScript StyleSheet) */
+
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flexGrow: 1,
+    marginLeft: 10,
+  }
+
+}))
+
+export default useStyles
